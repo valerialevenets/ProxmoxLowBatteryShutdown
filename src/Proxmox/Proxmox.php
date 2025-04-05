d86961e08e1327b797dd7dbc26f75cff92f03d72
Valerie <valeria.levenets94@gmail.com>
2025-04-05 19:53:14 +0300

    todos

--- src/Proxmox/Proxmox.php
+++ src/Proxmox/Proxmox.php
@@ -6,7 +6,7 @@ class Proxmox
 {
     public function __construct(private readonly ProxmoxNodeStatusProvider $nodeStatusProvider){}
 
-    public function shutdownNode(string $node): void //TODO change return type in the future
+    public function shutdownNode(string $node): void //TODO change return type in the future or throw exception if something wrong
     {
         $this->nodeStatusProvider->getNodeStatus($node)->nodeCmd('shutdown');
     }
